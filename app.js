fbf039a6188b82689517383121fd4757c4abb349
Pavel Reich <preich@static.144-76-94-145.clients.your-server.de>
2015-04-26 01:02:29 +0200

    added paypal

--- app.js
+++ app.js
@@ -5,12 +5,8 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 var braintree = require('braintree');
 /*
-var gateway = braintree.connect({
-environment: braintree.Environment.Sandbox,
-merchantId: "r44598y3bp9c9gxy",
-publicKey: "883n74q999ytt2s3",
-privateKey: "4de701d81a6c807fba184dee0d2e19f5"
-});
+npm install express body-parser braintree
+
 */
 
 var gateway = braintree.connect({
